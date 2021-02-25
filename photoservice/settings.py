3ceda750a964a7c8a8b260ff242c1cffc03fd345
Aldbrn <aldbrn.xxx@gmail.com>
2021-02-26 03:55:44 +0900

    fix

--- photoservice/settings.py
+++ photoservice/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import django_heroku
+import os
 from pathlib import Path
 
 
@@ -124,9 +125,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = str(BASE_DIR / "static")
+STATIC_URL = "/static/"
 
-STATICFILES_DIRS = [str(BASE_DIR / "static")]
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
 MEDIA_URL = "/media/"
 
